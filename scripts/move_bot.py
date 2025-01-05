551b6958dd991da444afad166ac51cddd463b828
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2025-01-04 23:13:02 -0800

    increased frequency to match the rate and added logging for errors

--- scripts/move_bot.py
+++ scripts/move_bot.py
@@ -4,16 +4,15 @@ from geometry_msgs.msg import Twist
 
 def move_robot():
     rospy.init_node('move_robot_node')
-    cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    rate = rospy.Rate(10)  # 10 Hz
+    cmd_vel_pub = rospy.Publisher('/diff_drive_controller/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(50)  # 50 Hz
 
     move_cmd = Twist()
-
-    # Set linear velocity (forward) and angular velocity (turning)
     move_cmd.linear.x = 0.5  # Move forward with 0.5 m/s
     move_cmd.angular.z = 0.0  # No rotation
 
     while not rospy.is_shutdown():
+        rospy.loginfo(f"Publishing cmd_vel: linear.x={move_cmd.linear.x}, angular.z={move_cmd.angular.z}")
         cmd_vel_pub.publish(move_cmd)
         rate.sleep()
 
